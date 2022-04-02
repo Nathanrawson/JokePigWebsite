7bc00715c866a4f6d9c2d3bbbe8d01cb62d8123a
NathanRawson <nathan_rawson@hotmail.com>
2022-04-02 15:03:05 +0100

    fixes

--- src/auth/isAuthenticted.js
+++ src/auth/isAuthenticted.js
@@ -1,11 +1,16 @@
 import MlApi from '../apis/MlApi'
 
 const IsAuthenitcated = async () => {
+    try{
     var response = await MlApi.get("/auth/isauthenticated");
     
     if(response.status === 200){
     return true;
     }
+    
     return false;
+}catch{
+    return false;
+}
 }
 export default IsAuthenitcated
